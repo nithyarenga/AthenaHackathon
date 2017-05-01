253106789fbc8e2e4f4ad134331a8a223e90ff85
root <root@bettertogether.athena.com>
2017-05-01 14:14:56 +0000

    Server

--- script/alchemy_analysis.py
+++ script/alchemy_analysis.py
@@ -12,8 +12,9 @@ natural_language_understanding = NaturalLanguageUnderstandingV1(
 def nl_processing(reqd_text):
     response = natural_language_understanding.analyze( text=reqd_text,
     features = [features.Entities(), features.Keywords(), features.Emotion(), features.Concepts(), features.Sentiment()])
-    return json.dumps(response, indent=2)
+    return response
 
 
 if __name__ == "__main__":
-    print(nl_processing("Bruce Banner is the Hulk and Bruce Wayne is BATMAN! Superman fears not Banner, but Wayne."))
+#    print(nl_processing("Bruce Banner is the Hulk and Bruce Wayne is BATMAN! Superman fears not Banner, but Wayne."))
+    print(nl_processing("Ever since I started seeing these people I have gotten better. It was slow at first(several months before results) because it took some time for me to build trust to be able to share my experiences in a vulnerable and truthful manner (for those that haven't been in therapy, it can be a real bitch to REALLY open yourself up to a stranger and hope they don't tear you apart and judge you, which counselors damn near never do, but the fear is there all the same). But after a lot of time, talking, and drugs (I am currently on depression, sleep, and mood meds and I haven't felt this normal since I left the military, which brings me to my next point)."))
--- server.py
+++ server.py
@@ -11,7 +11,10 @@ import athenahealthapi
 import datetime
 import StringIO
 from script.alchemy_analysis import nl_processing
+from script.personality_insight import personaility_analysis
 import random
+import os 
+import cPickle as pickle
 
 client = MongoClient()
 db = client.test
@@ -28,6 +31,46 @@ practiceid = '195900'
 api = athenahealthapi.APIConnection(version, key, secret, practiceid)
 
 app = Bottle()
+
+def scoring(personality_res):
+   result = personality_res['emotion']['document']['emotion']
+   score = 0
+   if result['anger'] > 0.8:
+       score += 4
+   elif result['anger'] > 0.5:
+       score += 3
+   elif result['anger'] > 0.3:
+       score += 2
+   if result['sadness'] > 0.8:
+       score += 4
+   elif result['sadness'] > 0.4:
+       score += 3
+   elif result['sadness'] > 0.2:
+       score += 2
+   if result['joy'] > 0.8:
+       score += -4
+   elif result['joy'] > 0.5:
+       score += -3
+   elif result['joy'] > 0.3:
+       score += -2
+   if result['fear'] > 0.8:
+       score += 3
+   elif result['fear'] > 0.5:
+       score += 2
+   elif result['fear'] > 0.3:
+       score += 1
+   if result['disgust'] > 0.8:
+       score += 3
+   elif result['disgust'] > 0.5:
+       score += 2
+   elif result['disgust'] > 0.3:
+       score += 1
+   if score < 1:
+       score = 1
+   elif score > 5:
+       score = 5
+   return score
+
 
 @app.hook('after_request')
 def enable_cors():
@@ -80,11 +123,22 @@ def patient_post():
     print new_patient_id
     return None
 
-@app.route('/getpatients')
+@app.route('/test')
 def manypatients():
     patient_id = {'departmentid': '1', 'primarydepartmentid': '1','primaryproviderid': '79','veteran': 'Y' }
     new_patient = api.GET('/patients', patient_id)
     return new_patient
+
+@app.route('/getpatients')
+def test():
+    patient_id = {'departmentid': '1', 'primarydepartmentid': '1','primaryproviderid': '79','veteran': 'Y' }
+    final = []
+    new_patient = api.GET('/patients', patient_id)
+    for i in new_patient["patients"]:
+       result = {"patientid":i["patientid"],"firstname":i["firstname"] ,"lastname": i["lastname"],"sex":i["sex"],"veteran":i["veteran"],"zip":i["zip"],"dob":i["dob"]}
+       final.append(result)
+    print final
+    return dumps(final)
 
 @app.route('/analysistext', method='POST')
 def analysiss():    
@@ -92,45 +146,166 @@ def analysiss():
     value = request.body.read()
     print value
     v = loads(value)
- #   result1 = nl_processing(v["text"])
-    result1 = {
-  "emotion": {
-    "document": {
-      "emotion": {
-        "anger": 0.052645,
-        "joy": 0.349187,
-        "sadness": 0.14315,
-        "fear": 0.258821,
-        "disgust": 0.049918
-      }
-    }
-  },
-  "sentiment": {
-    "document": {
-      "score": 0.0,
-      "label": "neutral"
-    }
-  },
-  "language": "en",
-  "entities": [],
-  "concepts": [],
-  "keywords": [
-    {
-      "relevance": 0.903313,
-      "text": "sun"
-    },
-    {
-      "relevance": 0.871856,
-      "text": "west"
-    }
-  ]
-}
+    TEST_MODE = False
+    filename = 'test_alchemy.pickle'
+    if os.path.isfile(os.path.join('./', filename)) and TEST_MODE:
+       print('Loading Pickle')
+       result = pickle.load(open(os.path.join('./', filename)))
+    else:
+       result = nl_processing(v["text"])
+       pickle.dump(result, open(os.path.join('./', filename), 'wb'))
+#    result1 = {
+#  "emotion": {
+#    "document": {
+#      "emotion": {
+#        "anger": 0.052645,
+#        "joy": 0.349187,
+#        "sadness": 0.14315,
+#        "fear": 0.258821,
+#        "disgust": 0.049918
+#      }
+#    }
+#  },
+#  "sentiment": {
+#    "document": {
+#      "score": 0.0,
+#      "label": "neutral"
+#    }
+#  },
+#  "language": "en",
+#  "entities": [],
+#  "concepts": [],
+#  "keywords": [
+#    {
+#      "relevance": 0.903313,
+#      "text": "sun"
+#    },
+#    {
+#      "relevance": 0.871856,
+#      "text": "west"
+#    }
+#  ]
+#}
     number = random.randint(1,5)
     millis = int(round(time.time() * 1000))
-    db.alerts.insert({"patient_id": v["patient_id"], "text": v["text"], "date" : millis , "scores": result1, "risk": {"name": number,"description":"low Risk"} })
+    number = scoring(result)
+    des = ""
+    if number == 1:
+       des = "Low Risk"
+    elif number == 2:
+       des = "Intermediate Low Risk"
+    elif number ==3 :
+       des = "Medium Risk"
+    elif number == 4:
+       des = "Intermediate High Risk"
+    else:
+       des = "High Risk"
+    db.alerts.insert({"patient_id": v["patient_id"], "text": v["text"], "date" : millis , "scores": result, "risk": {"name": number,"description": des} })
     something = db.alerts.find({}).sort("date",-1).limit(1)
     for doc in something:
         print doc
     	return json_util.dumps(doc)
 
-run(app, host='158.85.149.138', port=8080, debug=True)
+@app.route('/riskfactor', method='POST')
+def risk():
+   value = request.body.read()
+   print value
+   v = loads(value)
+   something = db.alerts.find({"patient_id": v["patient_id"]}).sort("date",-1).limit(1)
+   for doc in something:
+       result = {"risk": doc["risk"]["name"], "patient_id": v["patient_id"]}
+       return json_util.dumps(result)
+
+@app.route('/riskpatient', method='POST')
+def risking():
+    value = request.body.read()
+    print value
+    v = loads(value)
+    results = db.alerts.find({"patient_id":v["patient_id"]})
+    data = []
+    for doc in results:
+        created = doc["date"]
+        text = doc["risk"]["name"]
+        json = {"time": created, "risk": text}
+        data.append(json)
+    return json_util.dumps(data)
+
+@app.route('/messages')
+def risking():
+    results = db.alerts.find({"patient_id":"29380"}).sort("date",-1).limit(10)
+    data = []
+    for doc in results:
+        created = doc["date"]
+        text = doc["text"]
+        json = {"time": created, "text": text}
+        data.append(json)
+    return json_util.dumps(data)
+
+@app.route('/analysisvoice')
+def analysis2():
+
+    #value = request.body.read()
+    #v = loads(value)
+    #results = db.alerts.find({"patient_id":v["patient_id"]})
+    results = db.alerts.find({"patient_id":"29380"})
+
+    data = []
+    for doc in results:
+        created = doc["date"]
+        text = doc["text"]
+        json = {"created": created, "content": text}
+        data.append(json)
+
+    for doc in data:
+        print doc
+
+    something = personaility_analysis(data)
+    print something
+
+    return something
+
+#@app.route('/appointment')
+#def appt():
+#    patient_id = {'patientid': '29332' }
+#    new_patient = api.GET('/patients/:patientid/appointments', patient_id)
+
+@app.route('/getnote', method='POST')
+def get_note():
+
+    value = request.body.read()
+    print value
+    v = loads(value)
+
+    result = api.GET('/patients/' + v["patient_id"] + '/appointments', {"patientid":v["patient_id"], "practiceid":"195900", "limit":"1"})
+
+    print result
+
+    payload = {"appointmentid":result["appointments"][0]["appointmentid"]}
+    return api.GET('/appointments/' + result["appointments"][0]["appointmentid"] + '/notes', payload)
+
+@app.route('/postnote', method='POST')
+def post_note():
+
+    value = request.body.read()
+    print value
+    v = loads(value)
+
+    result = api.GET('/patients/' + v["patient_id"] + '/appointments', {"patientid":v["patient_id"], "practiceid":"195900", "limit":"1"})
+
+    print result
+
+    payload = {"appointmentid":result["appointments"][0]["appointmentid"], "notetext":v["notetext"]}
+    api.POST('/appointments/' + result["appointments"][0]["appointmentid"] + '/notes', payload)
+
+
+@app.route('/appointments', method='POST')
+def appointments():
+
+    value = request.body.read()
+    print value
+    v = loads(value)
+
+    return api.GET('/patients/' + v["patient_id"] + '/appointments', {"patientid":v["patient_id"], "practiceid":"195900", "limit":"1"})
+
+
+run(app,host="0.0.0.0", port=8080, debug=True)
